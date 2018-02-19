20a87322b9a26b6492abf989b0626974f29421a0
jfreek <jorgee.abud@gmail.com>
2018-02-18 21:22:33 -0600

    adding about url

--- mysite/index/urls.py
+++ mysite/index/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns = [url(r'^$', views.index, name='index')]
+urlpatterns = [
+	url(r'^index/$', views.index, name='index'),
+	url(r'^about/$', views.about, name="about"),
+			   ]
